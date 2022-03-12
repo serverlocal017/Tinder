21d914205f9b882a03dbac5b2b0859352dcf6bdd
serverlocal017 <79669115+serverlocal017@users.noreply.github.com>
2022-03-12 22:23:13 +0100

    Updated Swipe animation and structur

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import TinderSwiper from './src/components/TinderSwiper';
@@ -8,38 +8,71 @@ export interface IMock {
   source: string;
 }
 
-
 const mock: IMock[] = [
   {
     id: 1,
-    source: 'https://placeimg.com/240/240/any'
+    source: 'https://placeimg.com/240/280/any'
   },
   {
-    id: 2,
-    source: 'https://placeimg.com/240/240/any'
+    id: 2, 
+    source: 'https://placeimg.com/220/280/any'
   },
   {
     id: 3,
-    source: 'https://placeimg.com/240/240/any'
+    source: 'https://placeimg.com/280/280/any'
   },
   {
     id: 4,
-    source: 'https://placeimg.com/240/240/any'
-  }
+    source: 'https://placeimg.com/260/280/any'
+  },
+  {
+    id: 5,
+    source: 'https://placeimg.com/240/290/any'
+  },
 ];
+
+const data = [
+  {
+    id: 9, 
+    source: 'https://placeimg.com/280/340/any'
+  },
+  {
+    id: 10,
+    source: 'https://placeimg.com/290/350/any'
+  },
+  {
+    id: 11,
+    source: 'https://placeimg.com/300/360/any'
+  }
+]
 
 const renderItem = ({ id, source }: IMock) => {
   return (
-    <View style={{ borderWidth: 4, borderColor: '#fff'}}>
-      <Image source={{uri: source}} style={{height: 300, width: 300}} resizeMode='contain' />
+    <View style={{ borderWidth: 4, borderColor: '#fff', borderRadius: 4}}>
+      <Image source={{uri: source}} style={{height: 300, width: 300}} />
     </View>
   )
 }
 
 export default function App() {
+  
+  const [mockData, setMockData] = useState<IMock[]>(mock);
+
+  const removeItem = useCallback((id: number) => {
+    const findObjectFromIndex = mockData.find((el, i) => i === id);
+     mockData.length === 1 ? setMockData((curData) => curData.filter((el => el.id !== findObjectFromIndex?.id)).concat(data))  : setMockData((curData) => curData.filter((el => el.id !== findObjectFromIndex?.id)))
+  }, [mockData]);
+/* 
+  useEffect(() => {
+    mockData.length > 1 && setMockData((cur) => cur.concat(mock));
+  }, [mockData]);
+ */
+
+//  console.log(mockData);
+  
   return (
     <GestureHandlerRootView style={s.container}>
-      <TinderSwiper mock={mock} renderItem={(item) => renderItem(item)} />
+      <TinderSwiper mock={mockData} renderItem={(item) => renderItem(item)} swipedLeft={(id) => removeItem(id)} />
     </GestureHandlerRootView>
   )
 }
--- src/components/TinderSwiper.tsx
+++ src/components/TinderSwiper.tsx
@@ -1,26 +1,87 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Animated from 'react-native-reanimated'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Animated, { Extrapolate, interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 import { IMock } from '../../App'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 
 interface ITinder<T> {
   mock: T[],
   renderItem: (item: T) => React.ReactNode;
+  swipedLeft: (id: number) => void;
 }
 
-const TinderSwiper = ({ mock, renderItem }: ITinder<IMock>) => {
+const { width } = Dimensions.get('window');
+
+const TinderSwiper = ({ mock, renderItem, swipedLeft }: ITinder<IMock>) => {
+
+  const translateX = useSharedValue(0);
+
+  useEffect(() => {
+    translateX.value = 0;
+  }, [mock]);
+
+  const panGestureEvent = Gesture.Pan()
+    .onUpdate((e) => {
+      translateX.value = e.translationX;
+    })
+    .onEnd(() => {
+      if(translateX.value <= -width / 2) {
+        translateX.value = -width * 2;
+        runOnJS(swipedLeft)(mock.length - 1)
+      } else {
+        translateX.value = withTiming(0);
+      }
+ 
+    })
+
+    const rCard = useAnimatedStyle(() => {
+      const rotateZ = interpolate(translateX.value, [0, -width / 2], [0, -15]);
+      return {
+        transform: [{ translateX: translateX.value }, { rotateZ: `${rotateZ}deg` }]
+      }
+    });
+
+    const rScale = useAnimatedStyle(() => {
+      const scale = interpolate(translateX.value, [-width / 2, 0, width / 2], [1, 0, 1], Extrapolate.CLAMP);
+      const opacity = interpolate(translateX.value, [-width * 2, 0, width * 2], [1, 0.4, 1]);
+      return {
+        opacity,
+        transform: [{ scale }]
+      }
+    });
+    
+
   return (
     <Animated.View style={StyleSheet.absoluteFill}>
       <View style={{...StyleSheet.absoluteFillObject, justifyContent: 'center', alignItems: 'center'}}>
           {
-            mock.map((el, i) => (
-              <View key={i} style={s.imageContainer}>
-                <GestureDetector>
-                  {renderItem(el)}
-                </GestureDetector>
-              </View>
-            ))
+            mock.map((el, i) => {
+              
+            
+              if(i == mock.length - 1) {
+                return (
+                <Animated.View key={i} style={[s.imageContainer, rCard, { zIndex: 200 }]}>
+                  <GestureDetector gesture={panGestureEvent}>
+                    {renderItem(el)}
+                  </GestureDetector>
+                </Animated.View>
+                )
+              } else if(i === mock.length -2) {
+                return (
+                  <Animated.View key={i} style={[s.imageContainer, rScale]}>
+                
+                    {renderItem(el)}
+            
+                </Animated.View>
+                    )
+              } else {
+                <Animated.View key={i} style={[s.imageContainer, { opacity: 0 }]}>
+                
+                {renderItem(el)}
+        
+            </Animated.View>
+              }
+            })
           }
         </View>
     </Animated.View>
