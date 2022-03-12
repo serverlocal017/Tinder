754b41097f63d7daccfca09ceddbf09f02193680
serverlocal017 <79669115+serverlocal017@users.noreply.github.com>
2022-03-12 12:33:09 +0100

    RN-Gesture & Animated and folder created with Card.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 
+const mock = [
+  {
+    id: 1,
+    source: 'https://placeimg.com/240/240/any'
+  },
+  {
+    id: 2,
+    source: 'https://placeimg.com/240/240/any'
+  },
+  {
+    id: 3,
+    source: 'https://placeimg.com/240/240/any'
+  },
+  {
+    id: 4,
+    source: 'https://placeimg.com/240/240/any'
+  }
+];
+
 export default function App() {
   return (
     <View style={s.container}>
